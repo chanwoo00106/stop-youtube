760e4683eb0f24958a0ddcd47af4d5e97010805f
변찬우 <byun00106@gmail.com>
2024-08-03 11:57:16 +0900

    feat: add blocking access youtube

--- packages/background/index.ts
+++ packages/background/index.ts
@@ -1,11 +1,18 @@
-chrome.tabs.query({ url: "https://www.youtube.com/*" }, (tabs) => {
-  tabs.forEach((tab) => {
-    if (tab.id) chrome.tabs.remove(tab.id);
+// Youtube 접근 차단
+chrome.storage.local.get(["access-block"], (result) => {
+  if (result["access-block"] !== "true") return;
+
+  chrome.tabs.query({ url: "https://www.youtube.com/*" }, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab.id) chrome.tabs.remove(tab.id);
+    });
   });
 });
 
 chrome.tabs.onUpdated.addListener(async (tabId, moveInfo) => {
-  console.log("hello");
-  if (!moveInfo.url?.includes("https://www.youtube.com/")) return;
-  chrome.tabs.remove(tabId);
+  chrome.storage.local.get(["access-block"], (result) => {
+    if (result["access-block"] !== "true") return;
+    if (!moveInfo.url?.includes("https://www.youtube.com/")) return;
+    chrome.tabs.remove(tabId);
+  });
 });
--- packages/popup/index.ts
+++ packages/popup/index.ts
@@ -1,6 +1,19 @@
 console.log(document.body);
 
-const div = document.createElement("div");
-div.innerText = "Hello World";
+const accessBlock = document.querySelector(".access-block");
+if (accessBlock instanceof HTMLInputElement) {
+  chrome.storage.local.get(["access-block"], (result) => {
+    accessBlock.defaultChecked = result["access-block"] === "true";
+  });
 
-document.body.appendChild(div);
+  accessBlock?.addEventListener("click", () => {
+    chrome.storage.local.set({
+      "access-block": `${accessBlock.checked === true}`,
+    });
+  });
+}
+
+// const div = document.createElement("div");
+// div.innerText = "Hello World";
+//
+// document.body.appendChild(div);
