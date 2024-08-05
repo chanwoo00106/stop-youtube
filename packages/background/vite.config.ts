e848452b32659a8a5af8e11c0ad12adc309caf3a
변찬우 <byun00106@gmail.com>
2024-08-05 16:12:06 +0900

    fix: move public folder

--- packages/background/vite.config.ts
+++ packages/background/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: __dirname,
-  publicDir: "../../public",
+  publicDir: "./public",
   build: {
     outDir: "../../dist/background",
     lib: {
--- packages/popup/vite.config.ts
+++ packages/popup/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  publicDir: "../../public",
   base: "",
   root: __dirname,
   build: {
